4324bccd2a4af06ef4ff51fae79082c338682848
ajiajithkavi <161112469+ajiajithkavi@users.noreply.github.com>
2024-03-15 05:41:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request, session
 from datetime import timedelta
-
+import random
 app = Flask(__name__)
 app.secret_key = "hello"
 app.permanent_session_lifetime = timedelta(minutes=1)
